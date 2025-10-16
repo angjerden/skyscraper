2f6945324238d67d914d4a8afb9d38aec7b35ac3
Anders Gjerde <anders.gjerde@protonmail.com>
2025-10-16 21:27:58 +0200

    Added some attempted bmp writings of rooms - not working

--- scraper.cpp
+++ scraper.cpp
@@ -28,19 +28,31 @@ void Scraper::scrapeAssetsByRunningTheWholeDamnEngine() {
 
 // get hardcoded assets from the intro and elsewhere
 void Scraper::scrapeAssetsHardcoded() {
-    // virgin palette
     uint8* virginPal = _skyDisk->loadFile(60111);
-    // virgin screen
     uint8* virginScr = _skyDisk->loadFile(60110);
 
     Writer::writeBMP("virgin.bmp", virginScr, virginPal);
 
     uint8* shamanPal = _skyDisk->loadFile(59501);
-    uint8* shamanScr = _skyDisk->loadFile(59502);
+    // uint8* shamanScr = _skyDisk->loadFile(59502);
 
-    Writer::writeBMP("shaman.bmp", shamanScr, shamanPal);
+    // Writer::writeBMP("shaman.bmp", shamanScr, shamanPal);
     // linc mouse cursors
     uint8* lincCursors = _skyDisk->loadFile(60302);
+
+    uint8* file49 = _skyDisk->loadFile(49);
+    Writer::writeBMP("file49.bmp", file49, shamanPal);
+
+    uint8* startRoom = _skyDisk->loadFile(64);
+    uint8* startRoomPal = (uint8*)_skyCompact->fetchCpt(4316);
+    Writer::writeBMP("file64.bmp", startRoom, startRoomPal);
+
+    uint8* securityTerrace = _skyDisk->loadFile(92);
+    uint8* securityTerracePal = (uint8*)_skyCompact->fetchCpt(4317);
+    Writer::writeBMP("file92.bmp", securityTerrace, securityTerracePal);
+
+    uint8* sprite89 = _skyDisk->loadFile(89);
+    Writer::writeBMP("sprite89.bmp", sprite89, shamanPal);
 }
 
 void Scraper::writeDinnerTableToFile() {
